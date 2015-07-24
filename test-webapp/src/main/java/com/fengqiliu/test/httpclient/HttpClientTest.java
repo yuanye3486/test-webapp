40fcb284ba70f579b595e9ba3518f1215db76402
liufengqi <fengqiliu@gmail.com>
2015-07-24 17:56:11 +0800

    添加测试

--- test-webapp/src/main/java/com/fengqiliu/test/httpclient/HttpClientTest.java
+++ test-webapp/src/main/java/com/fengqiliu/test/httpclient/HttpClientTest.java
@@ -1,5 +1,6 @@
 package com.fengqiliu.test.httpclient;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -13,42 +14,66 @@ import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.time.DateUtils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HttpClientTest {
 	private static ObjectMapper objectMapper = new ObjectMapper();
-
-	public static void main(String[] args) throws IOException, ParseException {
+	
+	public static void main(String[] args) throws IOException, ParseException, InterruptedException {
+/*		Data data = new Data("dcm.group.1hour", "loc-windows", "dataRepertoryId=52,targetName=guonei10", 250, 1437634800, 3600, "GAUGE");
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(DateUtils.parseDate("2015-07-23 00", "yyyy-MM-dd HH"));
+		while(cal.getTime().before(new Date())){
+//			cal.add(Calendar.MINUTE, 1);
+			cal.add(Calendar.HOUR_OF_DAY, 1);
+			data.setTimestamp(cal.getTime().getTime()/1000);
+			addOne(data);
+		}*/
+		
+//		addMulti();
+		
 		addOne();
-
 	}
 	
 	public static void addOne() throws ParseException, HttpException, IOException{
-		String url = "http://192.168.73.128:1988/v1/push";
-		long dateTimestamp = DateUtils.parseDate("2015-7-21 17", "yyyy-MM-dd HH").getTime()/1000;
-		double value = 210d;
-		Data data = new Data("dcm.dbDataInc", "loc-vm-centos", "dbname=dc_chuantong_0819", value, dateTimestamp, 3600, "GAUGE");
+		String url = "http://192.168.73.128:1989/v1/push";
+//		long dateTimestamp = DateUtils.parseDate("2015-7-24 14", "yyyy-MM-dd HH").getTime()/1000;
+		long dateTimestamp = DateUtils.parseDate("2015-07-24 24", "yyyy-MM-dd HH").getTime()/1000;
+		double value = 250d;
+		Data data = new Data("dcm.group.1hour", "loc-windows", "dataRepertoryId=52,targetName=guonei10", value, dateTimestamp, 3600, "GAUGE");
 		String datajson = objectMapper.writeValueAsString(data);
 		datajson = "[" + datajson + "]";
 		post(url, datajson);
 	}
+	
+	public static void addOne(Data data) throws ParseException, HttpException, IOException{
+		String url = "http://192.168.73.128:1989/v1/push";
+		String datajson = objectMapper.writeValueAsString(data);
+		datajson = "[" + datajson + "]";
+		System.out.println(datajson);
+		post(url, datajson);
+	}
 
 	public static void addMulti() throws ParseException, HttpException, IOException{
-		String url = "http://192.168.73.128:1988/v1/push";
+		String url = "http://192.168.73.128:1989/v1/push";
 		List<Data> list = new ArrayList<Data>();
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(DateUtils.parseDate("2015-07-01 00:00:00", "yyyy-MM-dd HH:mm:ss"));
+		cal.setTime(DateUtils.parseDate("2015-01-01 00", "yyyy-MM-dd HH"));
 		
+		
+		Date endTime = DateUtils.parseDate("2015-01-02 00", "yyyy-MM-dd HH");
 		Random random = new Random();
-		while(cal.getTime().before(new Date())){
-			Data data = new Data("dcm.dbDataInc", "loc-vm-centos", "dbname=dc_chuantong_0819", random.nextInt(100000), cal.getTime().getTime()/1000, 3600, "GAUGE");	
+		while(cal.getTime().before(endTime)){
+			Data data = new Data("dcm.dbDataInc", "loc-windows", "dbname=dc_chuantong", random.nextInt(100000), cal.getTime().getTime()/1000, 3600, "GAUGE");	
 			cal.add(Calendar.HOUR_OF_DAY, 1);
 			list.add(data);
 			if(list.size() == 500){
 				String datajson = objectMapper.writeValueAsString(list);
-				System.out.println(datajson);
+				FileUtils.writeStringToFile(new File("C:\\Users\\Administrator\\Desktop\\muti.log"), datajson + "\n", true);
 				post(url, datajson);
 				list.clear();
 			}
@@ -56,22 +81,37 @@ public class HttpClientTest {
 		
 		if(list.size()>0){
 			String datajson = objectMapper.writeValueAsString(list);
-			System.out.println(datajson);
+			FileUtils.writeStringToFile(new File("C:\\Users\\Administrator\\Desktop\\muti.log"), datajson + "\n", true);
 			post(url, datajson);
 		}
+	}
+	
+	public static void post(String url, List<Data> list) throws HttpException, JsonProcessingException, IOException{
+		String jsonData = objectMapper.writeValueAsString(list);
+		List<String> dataList = new ArrayList<String>(1);
+		dataList.add(jsonData);
+		FileUtils.writeLines(new File("C:\\Users\\Administrator\\Desktop\\push.txt"), dataList, true);
+		
+//		try {
+//			Thread.sleep(10000L);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+		
+		post(url, jsonData);
 	}
 	
 	public static void post(String url, String json) throws HttpException, IOException{
 		HttpClient client = new HttpClient();
 		PostMethod post = new PostMethod(url);
-		System.out.println("push data = " + json);
+//		System.out.println("push data = " + json);
 		RequestEntity requestEntity = new StringRequestEntity(json, "application/json", "utf-8");
 		post.setRequestEntity(requestEntity);
 		client.executeMethod(post);
 		// 打印服务器返回的状态
-		System.out.println("Status = " + post.getStatusLine());
+//		System.out.println("Status = " + post.getStatusLine());
 		// 打印返回的信息
-		System.out.println("response info=" + post.getResponseBodyAsString());
+//		System.out.println("response info=" + post.getResponseBodyAsString());
 		// 释放连接
 		post.releaseConnection();
 	}
@@ -85,6 +125,9 @@ public class HttpClientTest {
 		private int step;
 		private String counterType;
 
+		public static final String GAUGE = "GAUGE";
+		public static final String COUNTER = "COUNTER";
+		
 		public Data() {
 			super();
 		}
